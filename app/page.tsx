794f13d5d6dd69539c033192024b4681e2974340
Rayel3gant <helloakash04@gmail.com>
2025-08-05 16:41:42 +0530

    move launchData table from page.tsx to separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
 'use client';
-
 import React, { useEffect, useState } from 'react';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-
-
 import {
   Select,
   SelectContent,
@@ -26,6 +15,7 @@ import Loader from '@/components/Loader';
 import { useAllLaunches } from '../hooks/useAllLaunches';
 import { useLaunchDetails } from '../hooks/useLaunchDetails';
 import Pagintaion from '@/components/Pagintaion';
+import DataTable from '@/components/DataTable';
 
 const Page = () => {
   const [launchData, setLaunchData] = useState<Launch[]>([]);
@@ -37,7 +27,6 @@ const Page = () => {
   const [loading,setLoading]=useState(false)
   const [currentRows,setCurrentRows]=useState<Launch[]>([])
  
-
   const { data: allLaunches, isStale, isFetching, refetch } = useAllLaunches();
   useEffect(() => {
     if (allLaunches) {
@@ -56,7 +45,7 @@ const Page = () => {
   
   const launchStatusFilter = (status: string) => {
     setLoading(true);
-    setCurrentPage(1); // Reset pagination on filter change
+    setCurrentPage(1);
 
     let filtered: Launch[] = [];
 
@@ -135,80 +124,44 @@ const Page = () => {
                     </button>
 
                   </div>
-                  <Table className="table-fixed w-full tableBorder rounded-md min-w-[1200px]">
-                    <TableHeader className="bg-[#F4F5F7] text-[#4B5563]">
-                      <TableRow>
-                        <TableHead className="w-[60px] text-center">No.</TableHead>
-                        <TableHead className="min-w-[300px] max-w-fit text-center">Launched (UTC)</TableHead>
-                        <TableHead className="min-w-[250px] max-w-fit text-center">Location</TableHead>
-                        <TableHead className="min-w-[150px] text-center">Mission</TableHead>
-                        <TableHead className="min-w-[120px] text-center">Orbit</TableHead>
-                        <TableHead className="min-w-[160px] text-center">Launch Status</TableHead>
-                        <TableHead className="min-w-[120px] text-center">Rocket</TableHead>
-                      </TableRow>
-                    </TableHeader>
 
-                    <TableBody className="text-[#1F2937] border-b-0">
-                      {currentRows.map((launch: Launch, index: number) => (
-                        <TableRow
-                          onClick={() => openLaunchDetailsModal(launch.id)}
-                          key={launch.id}
-                          className="cursor-pointer hover:bg-gray-50"
-                        >
-                          <TableCell className='text-center border-0'>{ index + 1}</TableCell>
-                          <TableCell className='text-center border-0'>{formatLaunchDate(launch.static_fire_date_utc!)}</TableCell>
-                          <TableCell className='text-center border-0'>{launch.location}</TableCell>
-                          <TableCell className='text-center border-0'>{launch.name}</TableCell>
-                          <TableCell className='text-center border-0'>{launch.orbit}</TableCell>
-                          <TableCell className='flex justify-center items-center border-0'>
-                            {launch.upcoming && (
-                              <span className="bg-[#FEF3C7] text-[#92400F] rounded-[20px] px-3 py-1 inline-block">
-                                Upcoming
-                              </span>
-                            )}
-                            {launch.success === true && !launch.upcoming && (
-                              <span className="bg-[#DEF7EC] text-[#03543F] rounded-[20px] px-3 py-1 inline-block">
-                                Success
-                              </span>
-                            )}
-                            {launch.success === false && !launch.upcoming && (
-                              <span className="bg-[#FDE2E1] text-[#981B1C] rounded-[20px] px-3 py-1 inline-block">
-                                Failed
-                              </span>
-                            )}
-                          </TableCell>
-                          <TableCell className='text-center border-0'>{launch.rocket}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
+                  <DataTable
+                    currentRows={currentRows}
+                    openLaunchDetailsModal={openLaunchDetailsModal}
+                  />
 
                   <Pagintaion 
                     launchData={launchData} 
                     currentPage={currentPage} 
                     setCurrentPage={setCurrentPage} 
                     setCurrentRows={setCurrentRows}
-                  />
-                    
-                  
+                  />                
                 </>
               )
             }
             </>
           )
         }
-
-        
-
       </div>       
       </div>
 
       {
-        openModal && singleLaunch && <LaunchModal setOpenModal={setOpenModal} data={singleLaunch} openModal={openModal} modalDataLoading={modalDataLoading}   />
+        openModal && singleLaunch && 
+          <LaunchModal 
+            setOpenModal={setOpenModal} 
+            data={singleLaunch} 
+            openModal={openModal} 
+            modalDataLoading={modalDataLoading}   
+          />
       }
 
       {
-        openDateModal && <DateModal  setOpenModal={setDateModal}  openModal={openDateModal} setFilteredData={setLaunchData} />
+        openDateModal && 
+          <DateModal  
+            setOpenModal={setDateModal}  
+            openModal={openDateModal} 
+            setFilteredData={setLaunchData}
+          />
       }
     </div>
   );
---
+++ components/DataTable.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Launch } from '@/lib/types';
+import { formatLaunchDate } from '@/lib/utils';
+
+const DataTable = ({currentRows , openLaunchDetailsModal}:
+  {
+    currentRows:Launch[],
+    openLaunchDetailsModal:(id:string)=>void
+  }) => {
+  return (
+    <div>
+      <Table className="table-fixed w-full tableBorder rounded-md min-w-[1200px]">
+        <TableHeader className="bg-[#F4F5F7] text-[#4B5563]">
+          <TableRow>
+            <TableHead className="w-[60px] text-center">No.</TableHead>
+            <TableHead className="min-w-[300px] max-w-fit text-center">Launched (UTC)</TableHead>
+            <TableHead className="min-w-[250px] max-w-fit text-center">Location</TableHead>
+            <TableHead className="min-w-[150px] text-center">Mission</TableHead>
+            <TableHead className="min-w-[120px] text-center">Orbit</TableHead>
+            <TableHead className="min-w-[160px] text-center">Launch Status</TableHead>
+            <TableHead className="min-w-[120px] text-center">Rocket</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody className="text-[#1F2937] border-b-0">
+          {currentRows.map((launch: Launch, index: number) => (
+            <TableRow
+              onClick={() => openLaunchDetailsModal(launch.id)}
+              key={launch.id}
+              className="cursor-pointer hover:bg-gray-50"
+            >
+              <TableCell className='text-center border-0'>{ index + 1}</TableCell>
+              <TableCell className='text-center border-0'>{formatLaunchDate(launch.static_fire_date_utc!)}</TableCell>
+              <TableCell className='text-center border-0'>{launch.location}</TableCell>
+              <TableCell className='text-center border-0'>{launch.name}</TableCell>
+              <TableCell className='text-center border-0'>{launch.orbit}</TableCell>
+              <TableCell className='flex justify-center items-center border-0'>
+                {launch.upcoming && (
+                  <span className="bg-[#FEF3C7] text-[#92400F] rounded-[20px] px-3 py-1 inline-block">
+                    Upcoming
+                  </span>
+                )}
+                {launch.success === true && !launch.upcoming && (
+                  <span className="bg-[#DEF7EC] text-[#03543F] rounded-[20px] px-3 py-1 inline-block">
+                    Success
+                  </span>
+                )}
+                {launch.success === false && !launch.upcoming && (
+                  <span className="bg-[#FDE2E1] text-[#981B1C] rounded-[20px] px-3 py-1 inline-block">
+                    Failed
+                  </span>
+                )}
+              </TableCell>
+              <TableCell className='text-center border-0'>{launch.rocket}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>    
+    </div>
+  )
+}
+
+export default DataTable
